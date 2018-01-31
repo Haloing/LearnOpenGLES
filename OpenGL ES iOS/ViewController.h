4a00b1805a0c393f637ca3d1477b51313d708bf6
Hao <xionghaocoding@163.com>
2018-01-31 16:55:04 +0800

    master

---
+++ OpenGL ES iOS/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  OpenGL ES iOS
+//
+//  Created by Mac OS X on 2018/1/31.
+//  Copyright © 2018年 Mac OS X. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <GLKit/GLKit.h>
+
+@interface ViewController : GLKViewController
+
+
+@end
+
